cec3b04364f24723435c953675eb480a41b4f853
salmeleh <salmeleh@gmail.com>
2015-12-22 11:37:38 -0600

    first commit

---
+++ FlickFinder/SleepingInTheLibrary/ViewController.swift
@@ -0,0 +1,149 @@
+//
+//  ViewController.swift
+//  SleepingInTheLibrary
+//
+//  Created by Jarrod Parkes on 1/26/15.
+//  Copyright (c) 2015 Udacity. All rights reserved.
+//
+
+import UIKit
+
+/* 1 - Define constants */
+let BASE_URL = "https://api.flickr.com/services/rest/"
+let METHOD_NAME = "flickr.galleries.getPhotos"
+let API_KEY = "981c84261b3ba500d2aa1e5dc3bec9a3"
+let GALLERY_ID = "5704-72157622566655097"
+let EXTRAS = "url_m"
+let DATA_FORMAT = "json"
+let NO_JSON_CALLBACK = "1"
+
+class ViewController: UIViewController {
+ 
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoTitle: UILabel!
+    @IBOutlet weak var searchPhraseTextField: UITextField!
+    @IBOutlet weak var latitudeTextField: UITextField!
+    @IBOutlet weak var longitudeTextField: UITextField!
+    
+    
+    @IBAction func touchGrabNewImageButton(sender: AnyObject) {
+        getImageFromFlickr()
+    }
+
+    func getImageFromFlickr() {
+        
+        /* 2 - API method arguments */
+        let methodArguments = [
+            "method": METHOD_NAME,
+            "api_key": API_KEY,
+            "gallery_id": GALLERY_ID,
+            "extras": EXTRAS,
+            "format": DATA_FORMAT,
+            "nojsoncallback": NO_JSON_CALLBACK
+        ]
+        
+        /* 3 - Initialize session and url */
+        let session = NSURLSession.sharedSession()
+        let urlString = BASE_URL + escapedParameters(methodArguments)
+        let url = NSURL(string: urlString)!
+        let request = NSURLRequest(URL: url)
+        
+        /* 4 - Initialize task for getting data */
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            /* 5 - Check for a successful response */
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                print("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                if let response = response as? NSHTTPURLResponse {
+                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
+                } else if let response = response {
+                    print("Your request returned an invalid response! Response: \(response)!")
+                } else {
+                    print("Your request returned an invalid response!")
+                }
+                return
+            }
+
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+            
+            /* 6 - Parse the data (i.e. convert the data to JSON and look for values!) */
+            let parsedResult: AnyObject!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            } catch {
+                parsedResult = nil
+                print("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            /* GUARD: Did Flickr return an error (stat != ok)? */
+            guard let stat = parsedResult["stat"] as? String where stat == "ok" else {
+                print("Flickr API returned an error. See error code and message in \(parsedResult)")
+                return
+            }
+            
+            /* GUARD: Are the "photos" and "photo" keys in our result? */
+            guard let photosDictionary = parsedResult["photos"] as? NSDictionary,
+                photoArray = photosDictionary["photo"] as? [[String: AnyObject]] else {
+                print("Cannot find keys 'photos' and 'photo' in \(parsedResult)")
+                return
+            }
+            
+            /* 7 - Generate a random number, then select a random photo */
+            let randomPhotoIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
+            let photoDictionary = photoArray[randomPhotoIndex] as [String: AnyObject]
+            let photoTitle = photoDictionary["title"] as? String /* non-fatal */
+            
+            /* GUARD: Does our photo have a key for 'url_m'? */
+            guard let imageUrlString = photoDictionary["url_m"] as? String else {
+                print("Cannot find key 'url_m' in \(photoDictionary)")
+                return
+            }
+            
+            /* 8 - If an image exists at the url, set the image and title */
+            let imageURL = NSURL(string: imageUrlString)
+            if let imageData = NSData(contentsOfURL: imageURL!) {
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.photoImageView.image = UIImage(data: imageData)
+                    self.photoTitle.text = photoTitle ?? "(Untitled)"
+                })
+            } else {
+                print("Image does not exist at \(imageURL)")
+            }
+        }
+        
+        /* 9 - Resume (execute) the task */
+        task.resume()
+    }
+
+    /* Helper function: Given a dictionary of parameters, convert to a string for a url */
+    func escapedParameters(parameters: [String : AnyObject]) -> String {
+        
+        var urlVars = [String]()
+        
+        for (key, value) in parameters {
+            
+            /* Make sure that it is a string value */
+            let stringValue = "\(value)"
+            
+            /* Escape it */
+            let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+            
+            /* Append it */
+            urlVars += [key + "=" + "\(escapedValue!)"]
+            
+        }
+        
+        return (!urlVars.isEmpty ? "?" : "") + urlVars.joinWithSeparator("&")
+    }
+}
