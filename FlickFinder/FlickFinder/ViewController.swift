b592e64d5e55954154cf18942316741eac40e655
salmeleh <salmeleh@gmail.com>
2016-01-14 13:55:00 -0600

    complete FF

--- FlickFinder/FlickFinder/ViewController.swift
+++ FlickFinder/FlickFinder/ViewController.swift
@@ -12,11 +12,25 @@ import UIKit
 
 let BASE_URL = "https://api.flickr.com/services/rest/"
 let METHOD_NAME = "flickr.photos.search"
-let API_KEY = "981c84261b3ba500d2aa1e5dc3bec9a3"
+let API_KEY = "ENTER_YOUR_API_KEY_HERE"
 let EXTRAS = "url_m"
 let SAFE_SEARCH = "1"
 let DATA_FORMAT = "json"
 let NO_JSON_CALLBACK = "1"
+let BOUNDING_BOX_HALF_WIDTH = 1.0
+let BOUNDING_BOX_HALF_HEIGHT = 1.0
+let LAT_MIN = -90.0
+let LAT_MAX = 90.0
+let LON_MIN = -180.0
+let LON_MAX = 180.0
+
+// MARK: - String Extension
+
+extension String {
+    func toDouble() -> Double? {
+        return NSNumberFormatter().numberFromString(self)?.doubleValue
+    }
+}
 
 // MARK: - ViewController: UIViewController
 
@@ -40,16 +54,21 @@ class ViewController: UIViewController {
         /* Added from student request -- hides keyboard after searching */
         self.dismissAnyVisibleKeyboards()
         
-        let methodArguments: [String: String!] = [
-            "method": METHOD_NAME,
-            "api_key": API_KEY,
-            "text": self.phraseTextField.text,
-            "safe_search": SAFE_SEARCH,
-            "extras": EXTRAS,
-            "format": DATA_FORMAT,
-            "nojsoncallback": NO_JSON_CALLBACK
-        ]
-        getImageFromFlickrBySearch(methodArguments)
+        if !self.phraseTextField.text!.isEmpty {
+            self.photoTitleLabel.text = "Searching..."
+            let methodArguments: [String: String!] = [
+                "method": METHOD_NAME,
+                "api_key": API_KEY,
+                "text": self.phraseTextField.text,
+                "safe_search": SAFE_SEARCH,
+                "extras": EXTRAS,
+                "format": DATA_FORMAT,
+                "nojsoncallback": NO_JSON_CALLBACK
+            ]
+            getImageFromFlickrBySearch(methodArguments)
+        } else {
+            self.photoTitleLabel.text = "Phrase Empty."
+        }
     }
     
     @IBAction func searchPhotosByLatLonButtonTouchUp(sender: AnyObject) {
@@ -57,16 +76,37 @@ class ViewController: UIViewController {
         /* Added from student request -- hides keyboard after searching */
         self.dismissAnyVisibleKeyboards()
         
-        let methodArguments: [String: String!] = [
-            "method": METHOD_NAME,
-            "api_key": API_KEY,
-            "bbox": convertToBBOX(),
-            "safe_search": SAFE_SEARCH,
-            "extras": EXTRAS,
-            "format": DATA_FORMAT,
-            "nojsoncallback": NO_JSON_CALLBACK
-        ]
-        getImageFromFlickrBySearch(methodArguments)
+        if !self.latitudeTextField.text!.isEmpty && !self.longitudeTextField.text!.isEmpty {
+            if validLatitude() && validLongitude() {
+                self.photoTitleLabel.text = "Searching..."
+                let methodArguments = [
+                    "method": METHOD_NAME,
+                    "api_key": API_KEY,
+                    "bbox": createBoundingBoxString(),
+                    "safe_search": SAFE_SEARCH,
+                    "extras": EXTRAS,
+                    "format": DATA_FORMAT,
+                    "nojsoncallback": NO_JSON_CALLBACK
+                ]
+                getImageFromFlickrBySearch(methodArguments)
+            } else {
+                if !validLatitude() && !validLongitude() {
+                    self.photoTitleLabel.text = "Lat/Lon Invalid.\nLat should be [-90, 90].\nLon should be [-180, 180]."
+                } else if !validLatitude() {
+                    self.photoTitleLabel.text = "Lat Invalid.\nLat should be [-90, 90]."
+                } else {
+                    self.photoTitleLabel.text = "Lon Invalid.\nLon should be [-180, 180]."
+                }
+            }
+        } else {
+            if self.latitudeTextField.text!.isEmpty && self.longitudeTextField.text!.isEmpty {
+                self.photoTitleLabel.text = "Lat/Lon Empty."
+            } else if self.latitudeTextField.text!.isEmpty {
+                self.photoTitleLabel.text = "Lat Empty."
+            } else {
+                self.photoTitleLabel.text = "Lon Empty."
+            }
+        }
     }
     
     // MARK: Life Cycle
@@ -145,21 +185,56 @@ class ViewController: UIViewController {
         return keyboardSize.CGRectValue().height
     }
     
+    // MARK: Lat/Lon Manipulation
     
-    //MARK: get lat/lon
-    func convertToBBOX() -> String {
-        /*The 4 values represent the bottom-left corner of the box and the top-right corner,
-        minimum_longitude, minimum_latitude, maximum_longitude, maximum_latitude.*/
-        let minimum_longitude: String = String(format: "%f", Float(longitudeTextField.text!)! - 0.011)
-        let minimum_lattitude: String = String(format: "%f", Float(latitudeTextField.text!)! - 0.011)
-        let maximum_longitude: String = String(format: "%f", Float(longitudeTextField.text!)! + 0.011)
-        let maximum_lattitude: String = String(format: "%f", Float(latitudeTextField.text!)! + 0.011)
-        return minimum_longitude + "," + minimum_lattitude + "," + maximum_longitude + "," + maximum_lattitude
+    func createBoundingBoxString() -> String {
+        
+        let latitude = (self.latitudeTextField.text! as NSString).doubleValue
+        let longitude = (self.longitudeTextField.text! as NSString).doubleValue
+        
+        /* Fix added to ensure box is bounded by minimum and maximums */
+        let bottom_left_lon = max(longitude - BOUNDING_BOX_HALF_WIDTH, LON_MIN)
+        let bottom_left_lat = max(latitude - BOUNDING_BOX_HALF_HEIGHT, LAT_MIN)
+        let top_right_lon = min(longitude + BOUNDING_BOX_HALF_HEIGHT, LON_MAX)
+        let top_right_lat = min(latitude + BOUNDING_BOX_HALF_HEIGHT, LAT_MAX)
+        
+        return "\(bottom_left_lon),\(bottom_left_lat),\(top_right_lon),\(top_right_lat)"
     }
     
+    /* Check to make sure the latitude falls within [-90, 90] */
+    func validLatitude() -> Bool {
+        if let latitude : Double? = self.latitudeTextField.text!.toDouble() {
+            if latitude < LAT_MIN || latitude > LAT_MAX {
+                return false
+            }
+        } else {
+            return false
+        }
+        return true
+    }
+    
+    /* Check to make sure the longitude falls within [-180, 180] */
+    func validLongitude() -> Bool {
+        if let longitude : Double? = self.longitudeTextField.text!.toDouble() {
+            if longitude < LON_MIN || longitude > LON_MAX {
+                return false
+            }
+        } else {
+            return false
+        }
+        return true
+    }
+    
+    func getLatLonString() -> String {
+        let latitude = (self.latitudeTextField.text! as NSString).doubleValue
+        let longitude = (self.longitudeTextField.text! as NSString).doubleValue
+        
+        return "(\(latitude), \(longitude))"
+    }
     
     // MARK: Flickr API
     
+    /* Function makes first request to get a random page, then it makes a request to get an image with the random page */
     func getImageFromFlickrBySearch(methodArguments: [String : AnyObject]) {
         
         let session = NSURLSession.sharedSession()
@@ -215,13 +290,87 @@ class ViewController: UIViewController {
                 return
             }
             
+            /* GUARD: Is "pages" key in the photosDictionary? */
+            guard let totalPages = photosDictionary["pages"] as? Int else {
+                print("Cannot find key 'pages' in \(photosDictionary)")
+                return
+            }
+            
+            /* Pick a random page! */
+            let pageLimit = min(totalPages, 40)
+            let randomPage = Int(arc4random_uniform(UInt32(pageLimit))) + 1
+            self.getImageFromFlickrBySearchWithPage(methodArguments, pageNumber: randomPage)
+        }
+        
+        task.resume()
+    }
+    
+    func getImageFromFlickrBySearchWithPage(methodArguments: [String : AnyObject], pageNumber: Int) {
+        
+        /* Add the page to the method's arguments */
+        var withPageDictionary = methodArguments
+        withPageDictionary["page"] = pageNumber
+        
+        let session = NSURLSession.sharedSession()
+        let urlString = BASE_URL + escapedParameters(withPageDictionary)
+        let url = NSURL(string: urlString)!
+        let request = NSURLRequest(URL: url)
+        
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                print("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                if let response = response as? NSHTTPURLResponse {
+                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
+                } else if let response = response {
+                    print("Your request returned an invalid response! Response: \(response)!")
+                } else {
+                    print("Your request returned an invalid response!")
+                }
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+            
+            /* Parse the data! */
+            let parsedResult: AnyObject!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            } catch {
+                parsedResult = nil
+                print("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            /* GUARD: Did Flickr return an error (stat != ok)? */
+            guard let stat = parsedResult["stat"] as? String where stat == "ok" else {
+                print("Flickr API returned an error. See error code and message in \(parsedResult)")
+                return
+            }
+            
+            /* GUARD: Is the "photos" key in our result? */
+            guard let photosDictionary = parsedResult["photos"] as? NSDictionary else {
+                print("Cannot find key 'photos' in \(parsedResult)")
+                return
+            }
+            
             /* GUARD: Is the "total" key in photosDictionary? */
-            guard let totalPhotos = (photosDictionary["total"] as? NSString)?.integerValue else {
+            guard let totalPhotosVal = (photosDictionary["total"] as? NSString)?.integerValue else {
                 print("Cannot find key 'total' in \(photosDictionary)")
                 return
             }
             
-            if totalPhotos > 0 {
+            if totalPhotosVal > 0 {
                 
                 /* GUARD: Is the "photo" key in photosDictionary? */
                 guard let photosArray = photosDictionary["photo"] as? [[String: AnyObject]] else {
@@ -240,12 +389,21 @@ class ViewController: UIViewController {
                 }
                 
                 let imageURL = NSURL(string: imageUrlString)
-                
                 if let imageData = NSData(contentsOfURL: imageURL!) {
                     dispatch_async(dispatch_get_main_queue(), {
+                        
                         self.defaultLabel.alpha = 0.0
                         self.photoImageView.image = UIImage(data: imageData)
-                        self.photoTitleLabel.text = photoTitle ?? "(Untitled)"
+                        
+                        if methodArguments["bbox"] != nil {
+                            if let photoTitle = photoTitle {
+                                self.photoTitleLabel.text = "\(self.getLatLonString()) \(photoTitle)"
+                            } else {
+                                self.photoTitleLabel.text = "\(self.getLatLonString()) (Untitled)"
+                            }
+                        } else {
+                            self.photoTitleLabel.text = photoTitle ?? "(Untitled)"
+                        }
                     })
                 } else {
                     print("Image does not exist at \(imageURL)")
