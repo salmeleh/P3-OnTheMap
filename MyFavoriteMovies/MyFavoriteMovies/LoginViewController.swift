abf74683e168d3ec4d7f141789a7b6918397db77
salmeleh <salmeleh@gmail.com>
2016-01-21 16:04:56 -0600

    completed getUserID

--- MyFavoriteMovies/MyFavoriteMovies/LoginViewController.swift
+++ MyFavoriteMovies/MyFavoriteMovies/LoginViewController.swift
@@ -260,13 +260,13 @@ class LoginViewController: UIViewController {
     }
     
     func getSessionID(requestToken: String) {
+        
         /* TASK: Get a session ID, then store it (appDelegate.sessionID) and get the user's id */
+        
         /* 1. Set the parameters */
-        let methodParameters: [String: String!] = [
+        let methodParameters = [
             "api_key": appDelegate.apiKey,
-            "request_token": requestToken,
-            "username": self.usernameTextField.text,
-            "password": self.passwordTextField.text
+            "request_token": requestToken
         ]
         
         /* 2. Build the URL */
@@ -283,7 +283,7 @@ class LoginViewController: UIViewController {
             /* GUARD: Was there an error? */
             guard (error == nil) else {
                 dispatch_async(dispatch_get_main_queue()) {
-                    self.debugTextLabel.text = "Login Failed (getSessionID Failed - 1)."
+                    self.debugTextLabel.text = "Login Failed (Session ID)."
                 }
                 print("There was an error with your request: \(error)")
                 return
@@ -323,20 +323,20 @@ class LoginViewController: UIViewController {
                 return
             }
             
-            /* GUARD: Is the "success" key in parsedResult? */
+            /* GUARD: Is the "sessionID" key in parsedResult? */
             guard let sessionID = parsedResult["session_id"] as? String else {
                 dispatch_async(dispatch_get_main_queue()) {
-                    self.debugTextLabel.text = "Login Failed (getSessionID Failed - 2)."
+                    self.debugTextLabel.text = "Login Failed (Session ID)."
                 }
-                print("Login failed.")
+                print("Cannot find key 'sessionID' in \(parsedResult)")
                 return
             }
             
-        
-        /* 6. Use the data! */
-        print(sessionID)
+            /* 6. Use the data! */
+            self.appDelegate.sessionID = sessionID
+            self.getUserID(self.appDelegate.sessionID!)
         }
-    
+        
         /* 7. Start the request */
         task.resume()
     }
@@ -345,12 +345,83 @@ class LoginViewController: UIViewController {
         
         /* TASK: Get the user's ID, then store it (appDelegate.userID) for future use and go to next view! */
         /* 1. Set the parameters */
+        let methodParameters: [String: String!]  = [
+            "api_key": appDelegate.apiKey,
+            "session_id": self.appDelegate.sessionID
+        ]
+    
         /* 2. Build the URL */
+        let urlString = appDelegate.baseURLSecureString + "account" + appDelegate.escapedParameters(methodParameters)
+        let url = NSURL(string: urlString)!
+        
         /* 3. Configure the request */
+        let request = NSMutableURLRequest(URL: url)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
         /* 4. Make the request */
-        /* 5. Parse the data */
-        /* 6. Use the data! */
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.debugTextLabel.text = "Login Failed (getUserID-1)."
+                }
+                print("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                if let response = response as? NSHTTPURLResponse {
+                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
+                } else if let response = response {
+                    print("Your request returned an invalid response! Response: \(response)!")
+                } else {
+                    print("Your request returned an invalid response!")
+                }
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+            
+            /* 5. Parse the data */
+            let parsedResult: AnyObject!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            } catch {
+                parsedResult = nil
+                print("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            /* GUARD: Did TheMovieDB return an error? */
+            guard (parsedResult.objectForKey("status_code") == nil) else {
+                print("TheMovieDB returned an error. See the status_code and status_message in \(parsedResult)")
+                return
+            }
+            
+            /* GUARD: Is the "sessionID" key in parsedResult? */
+            guard let userID = parsedResult["id"] as? Int else {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.debugTextLabel.text = "Login Failed (getUserID-2)."
+                }
+                print("Cannot find key 'sessionID' in \(parsedResult)")
+                return
+            }
+            
+            /* 6. Use the data! */
+            self.appDelegate.userID = userID
+            print(userID)
+        
+        }
+        
         /* 7. Start the request */
+        task.resume()
+
     }
 }
 
@@ -456,4 +527,3 @@ extension LoginViewController {
         return keyboardSize.CGRectValue().height
     }
 }
-
