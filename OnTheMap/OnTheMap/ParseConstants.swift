bf4cdb18b27745708db71c794104113191e2fa32
salmeleh <salmeleh@gmail.com>
2016-02-11 15:18:42 -0600

    lol incorrect appID

--- OnTheMap/OnTheMap/ParseConstants.swift
+++ OnTheMap/OnTheMap/ParseConstants.swift
@@ -12,8 +12,8 @@ extension ParseClient {
     
     //MARK: Constants
     struct Constants {
-        static let applicationID: String = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8yeOdnAbIr"
-        static let APIKey: String = "QuWThTdiRmTux3YaDseYSEpUKo7aBYM737yKd4gY"
+        static let applicationID: String = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+        static let APIKey: String = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
         static let baseSecureURL: String = "https://api.parse.com/1/classes/StudentLocation"
     }
     
